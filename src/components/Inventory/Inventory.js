d6d54fd244d6210f4cac4d496acaba00aba55181
sayonsadh <sadhkhan1999@gmail.com>
2021-09-30 10:10:10 -0700

    updated the inventory

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Hey, i am sayen hossain sadh!! orofe Sayon Sadh</h1>
+        </div>
+    );
+};
+
+export default Inventory;
